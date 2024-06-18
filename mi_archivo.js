6f9d794fc6f2864aea2d1e20e43f849a14d8c313
SergioCR1912 <sergio_camram_for@hotmail.com>
2024-06-18 00:41:32 -0600

    Update mi_archivo.js #2

--- mi_archivo.js
+++ mi_archivo.js
@@ -1,2 +1,4 @@
-console.log(‘hola mundo’)
-Este es un ejercicio de uso de git y github
+alert("hola mundo");
+//Este es un ejercicio de uso de git y github
+alert("hola mundo_editado");
+console.log("cambios en el archivo");
