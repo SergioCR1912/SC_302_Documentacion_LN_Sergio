dd607b6b52e905ff9d19ee7b302c4142e31546dc
SergioCR1912 <sergio_camram_for@hotmail.com>
2024-06-18 00:51:13 -0600

    creacion de text file respuestas diapo8lab2

---
+++ respuestasdiapositiva8.js
@@ -0,0 +1,3 @@
+// CALENDARIO DE JQUERY https://github.com/kylestetz/CLNDR
+// MAYOR ESTRELLAS https://github.com/AUTOMATIC1111/stable-diffusion-webui 
+// DESARROLLADOR #1 EN TRENDING ES https://github.com/doronz88, no pude ver quien tenia mas desarrolladores porque me hubiera tenido que meter 1 por 1 a ver cuantos eran, solo salen los top 5 en todos
